b23eae1bc4f6f0f3d9c4ca9660b5eb3fb176a881
d21ce176 <97421522+d21ce176@users.noreply.github.com>
2022-01-27 09:07:40 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,12 @@
+N = int(input())
+KEY = map(int, input().split())
+KEY = sorted(KEY)
+
+for i in range(len(KEY)):
+    if(i != len(KEY)-1):
+        if(KEY[i]!=KEY[i-1] and KEY[i]!=KEY[i+1]):
+            print(KEY[i])
+            break;
+    else:
+        print(KEY[i])
+print("this program is prepared by om and id :d21ce176")  
